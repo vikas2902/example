4a45e52c35ff0f4547075e09113d85defaf4aaad
unknown <vikasgupta1624@gmail.com>
2020-07-01 03:21:07 +0530

    i

--- hello.py
+++ hello.py
@@ -1,3 +1,7 @@
 
 
-print("Hello Github")
+#Hi, This notebook will be used for the capstone project in data science
+
+import pandas as pd
+import numpy as np
+print("Hello Capstone Project Course!")
